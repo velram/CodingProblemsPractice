6f4296cf7729361c1ccdee90353a86101439fd50
Velmurugan Moorthy <velmurugan1993@gmail.com>
2019-09-30 15:04:02 +0530

    Vel | Quicksort | code refactored - merge result array logic moved into separate method

--- src/com/hackerrank/practice/algorithms/sorting/quicksort/PartitionManager.java
+++ src/com/hackerrank/practice/algorithms/sorting/quicksort/PartitionManager.java
@@ -20,34 +20,30 @@ public class PartitionManager {
 		int[] middleArray = new int[inputArray.length];
 		int leftArraySize = 0; 
 		int rightArraySize = 0; 
-		int equalValueArraySize = 0; 
+		int medianArraySize = 0; 
 		int pivot = inputArray[0];
+		
 		for(int loopIndex = 0 ; loopIndex < inputArray.length ; loopIndex++) {
 			
 			if(inputArray[loopIndex] < pivot) {
 				leftArray[leftArraySize++] = inputArray[loopIndex];
 			}
-			
-			else if(inputArray[loopIndex] > pivot) {
+		}	
+		
+		for(int loopIndex = 0 ; loopIndex < inputArray.length ; loopIndex++) {
+			if(inputArray[loopIndex] > pivot) {
 				rightArray[rightArraySize++] = inputArray[loopIndex];
 			}
-			
-			else if (inputArray[loopIndex] == pivot) {
-				middleArray[equalValueArraySize++] = inputArray[loopIndex];				
+		}
+		
+		for(int loopIndex = 0 ; loopIndex < inputArray.length ; loopIndex++) {
+			if (inputArray[loopIndex] == pivot) {
+				middleArray[medianArraySize++] = inputArray[loopIndex];				
 			}
 		}		
 		
-		for(int loopIndex = 0 ; loopIndex < leftArraySize ; loopIndex++) {
-			resultArray[loopIndex] = leftArray[loopIndex];
-		}
-		int tempArrayLoopIndex = 0;
-		for(int loopIndex = leftArraySize ; loopIndex < (leftArraySize + equalValueArraySize) ; loopIndex++, tempArrayLoopIndex++) {
-			resultArray[loopIndex] = middleArray[tempArrayLoopIndex];
-		}
-		tempArrayLoopIndex = 0;
-		for(int loopIndex = (leftArraySize + equalValueArraySize) ; loopIndex < (leftArraySize + equalValueArraySize + rightArraySize) ; loopIndex++, tempArrayLoopIndex++) {
-			resultArray[loopIndex] = rightArray[tempArrayLoopIndex];
-		}
+		resultArray = mergePartitions(leftArray, rightArray, middleArray, leftArraySize, rightArraySize,
+				medianArraySize);
 		// resultArray = populateResultArray(leftArray,leftArraySize,resultArray);
 		
 		
@@ -60,6 +56,23 @@ public class PartitionManager {
 		System.out.println("\n Printing resultArray array");
 		printArray(resultArray);	
 		
+		return resultArray;
+	}
+
+	private int[] mergePartitions(int[] leftArray, int[] rightArray, int[] middleArray,
+			int leftArraySize, int rightArraySize, int equalValueArraySize) {
+		int[] resultArray = new int[leftArraySize + rightArraySize + equalValueArraySize];
+		for(int loopIndex = 0 ; loopIndex < leftArraySize ; loopIndex++) {
+			resultArray[loopIndex] = leftArray[loopIndex];
+		}
+		int tempArrayLoopIndex = 0;
+		for(int loopIndex = leftArraySize ; loopIndex < (leftArraySize + equalValueArraySize) ; loopIndex++, tempArrayLoopIndex++) {
+			resultArray[loopIndex] = middleArray[tempArrayLoopIndex];
+		}
+		tempArrayLoopIndex = 0;
+		for(int loopIndex = (leftArraySize + equalValueArraySize) ; loopIndex < (leftArraySize + equalValueArraySize + rightArraySize) ; loopIndex++, tempArrayLoopIndex++) {
+			resultArray[loopIndex] = rightArray[tempArrayLoopIndex];
+		}
 		return resultArray;
 	}
 
