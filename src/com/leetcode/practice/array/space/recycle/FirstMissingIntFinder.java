3916e25aca7ffecfb5c9c704420369f3b0c18e1c
Velmurugan Moorthy <velmurugan1993@gmail.com>
2019-11-29 12:56:52 +0530

    Vel | First missing positive integer - Leetcode - accepted solution

--- src/com/leetcode/practice/array/space/recycle/FirstMissingIntFinder.java
+++ src/com/leetcode/practice/array/space/recycle/FirstMissingIntFinder.java
@@ -1,7 +1,11 @@
 package com.leetcode.practice.array.space.recycle;
 
-import java.util.Arrays;
-
+/**
+ * @author Velmurugan Moorthy
+ * 28.11.2019
+ * Find first missing positive integer
+ * Question URL : https://leetcode.com/problems/first-missing-positive/submissions/
+ */
 public class FirstMissingIntFinder {
     public static void main(String[] args){
 
@@ -9,63 +13,71 @@ public class FirstMissingIntFinder {
 
         //int[] inputArray = {0,0,0}; // Test case #1 - All zeros - PASS
         //int[] inputArray = {1,1,1};   // Test case #2 - All 1's (duplicate value) - PASS
-        //int[] inputArray = {-1,-2, -3};   // Test case #3 - All unique negative values - PASS
+        int[] inputArray = {-1,-2, -3};   // Test case #3 - All unique negative values - PASS
         //int[] inputArray = {1,2,3};   // Test case #4 - All unique negative values - PASS
         //int[] inputArray = {1};   // Test case #5 - Only one value present and it's "1" - PASS
         //int[] inputArray = {0};   // Test case #6 - Only one value present and it's "0" - PASS
-        int[] inputArray = {-1};   // Test case #7 - Only one value present and it's "-1"
+        //int[] inputArray = {-1};   // Test case #7 - Only one value present and it's "-1"
         //int[] inputArray = {-32};   // Test case #8 - Only one value present and it's negative value
         //int[] inputArray = {7,8,9,11,12};   // Test case #9 - Only positive values (unique) - missing element - PASS
-        solutionObject.firstMissingPositive(inputArray);
+        //int[] inputArray = {0,2,2,1,1}; //PASS
+        //int[] inputArray = {1,1,1,1,2,2,5,0}; //PASS
+        int firstMissingPositiveInt = solutionObject.firstMissingPositive(inputArray);
+        //System.out.println("First missing positive int : "+firstMissingPositiveInt);
 
     }
 }
 
 class Solution {
+    public int firstMissingPositive(int[] inputArray) {
 
-    public int firstMissingPositive(int[] nums) {
+        int index = 0;
+        int arraySize = inputArray.length;
+        //Iterate thru the array
+        while (index < arraySize ){
 
-        int firstMissingPositiveInt = 0;
-        //Step #1 - sort array
-        Arrays.sort(nums);
-        displayArray(nums);
+            //Negative integers - Ignore & move to next element
+            if(inputArray[index] <= 0 ){
+                index++;
+            }
+            else{ //Positive Integers
 
-        //Step #2 - Create new array with sorted postive ints oly
-        int[] sortedPositiveArray = populateSortedArray(nums);
-        displayArray(sortedPositiveArray);
+                //Beyond array index - Ignore & move to next element
+                if(inputArray[index] >= arraySize+1){
+                    index++;
+                }
+                //Number in correct index - Ignore & move to next element
+                else if(inputArray[index] == index + 1){
+                    index++;
+                }
+                //Duplicate element & original already exists in correct index - Ignore & move to next element
+                else if(inputArray[index] == inputArray[inputArray[index] - 1]){
+                    index++;
+                }
 
-        //Step #3 - Compare values from 1 to n
-        firstMissingPositiveInt = findFirstMissingPositiveInt(sortedPositiveArray);
+                else { // Element is unique and is in an incorrect place do swap
+                    swap(inputArray, index, inputArray[index]-1);
+                }
 
-        System.out.println("First missing positive integer is : "+firstMissingPositiveInt);
-        return firstMissingPositiveInt;
-    }
-
-    private int findFirstMissingPositiveInt(int[] sortedPositiveArray) {
-        int firstMissingPositiveInt = 0;
-        int arraySize = sortedPositiveArray.length;
-        for (int loopIndex = 0; loopIndex < arraySize; loopIndex++) {
-            if((loopIndex + 1) != sortedPositiveArray[loopIndex]){
-                firstMissingPositiveInt = (loopIndex + 1);
-                break;
             }
         }
-        // If no value is missing return last index + 1
-        if(firstMissingPositiveInt == 0){
-            firstMissingPositiveInt = sortedPositiveArray[arraySize-1] + 1;
+
+        index = 0;
+
+        while (index < arraySize  && inputArray[index] == (index + 1)  ){
+            //System.out.println("index : "+index + " inputArray[index] : "+inputArray[index]);
+            index++;
         }
-        return firstMissingPositiveInt;
+
+
+        return index+1;
     }
 
-    private int[] populateSortedArray(int[] nums) {
-        int[] positiveValArray = new int[nums.length];
-        int positiveIntCount = 0;
-        for (int loopIndex = 0; loopIndex < nums.length; loopIndex++) {
-            if(nums[loopIndex] > 0){
-                positiveValArray[positiveIntCount++] = nums[loopIndex];
-            }
-        }
-        return positiveValArray;
+    private void swap(int[] inputArray, int sourceIndex, int destinationIndex) {
+        //System.out.println("Entering swap | sourceIndex : " + sourceIndex + " destinationIndex : "+destinationIndex);
+        int tempVariable = inputArray[sourceIndex];
+        inputArray[sourceIndex] = inputArray[destinationIndex];
+        inputArray[destinationIndex] = tempVariable;
     }
 
     private void displayArray(int[] nums) {
