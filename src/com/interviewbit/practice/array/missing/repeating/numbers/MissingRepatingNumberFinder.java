937151ecad06c350592721b7ffdff16870bcd449
Velmurugan Moorthy <velmurugan1993@gmail.com>
2019-12-24 08:53:17 +0530

    Vel | InterviewBit - Missing & Repeating number - initial commit

---
+++ src/com/interviewbit/practice/array/missing/repeating/numbers/MissingRepatingNumberFinder.java
@@ -0,0 +1,27 @@
+package com.interviewbit.practice.array.missing.repeating.numbers;
+
+/**
+ * @author Velmurugan Moorthy
+ * 23.12.2019
+ * Question URL : https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+ */
+public class MissingRepatingNumberFinder {
+    public static void main(String[] args) {
+
+
+        int[] inputArray = null;
+        int[] result = new int[2];
+
+        MissingRepeatingNumberSolution solutionObject = new MissingRepeatingNumberSolution();
+
+        result = solutionObject.repeatedNumber(inputArray);
+    }
+}
+
+ class MissingRepeatingNumberSolution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int[] repeatedNumber(final int[] A) {
+
+        return null;
+    }
+}
